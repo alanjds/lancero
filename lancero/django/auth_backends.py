e23d6a0253a32c197eccc6476a921ddb13515722
Alan Justino <alan.justino@yahoo.com.br>
2012-06-02 15:14:23 -0300

    Backend name changed

--- lancero/django/auth_backends.py
+++ lancero/django/auth_backends.py
@@ -3,7 +3,7 @@ from django.contrib.auth.backends import ModelBackend
 from django.core.validators import email_re
 
 
-class EmailBackend(ModelBackend):
+class EmailModelBackend(ModelBackend):
     def authenticate(self, username=None, password=None):
         #If username is an email address, then try to pull it up
         if email_re.search(username):
