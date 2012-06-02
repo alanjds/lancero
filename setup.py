2d4ab743a7ff26f9aa74e38fd68678cc2bd34867
Alan Justino <alan.justino@yahoo.com.br>
2012-06-02 15:08:21 -0300

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = "lancero",
-    version = "0.1",
+    version = "0.2",
     packages = ['lancero',
                 'lancero.misc',
                 'lancero.django',
