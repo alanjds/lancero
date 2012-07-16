ecbd7dc5cc411e1247d5234437bee53bc6f5e797
Alan Justino <alan.justino@yahoo.com.br>
2012-07-16 01:31:42 -0300

    4 spaces please.

--- lancero/django/urls.py
+++ lancero/django/urls.py
@@ -21,28 +21,28 @@ def secure_required(view_func):
 
 #From: http://noah.heroku.com/past/2010/2/19/django_http_basic_auth/
 class http_auth_required(object):
-  """
-  A decorator to handle basic HTTP authentication. Takes a dictionary of
-  username: password pairs to authenticate against.
-  """
-  def __init__(self, credentials):
-    self.credentials = credentials
+    """
+    A decorator to handle basic HTTP authentication. Takes a dictionary of
+    username: password pairs to authenticate against.
+    """
+    def __init__(self, credentials):
+      self.credentials = credentials
 
-  def __call__(self, view):
-    def inner(request, *args, **kwargs):
-      # header indicates login attempt
-      if request.META.has_key('HTTP_AUTHORIZATION'):
-        auth = request.META['HTTP_AUTHORIZATION'].split()
-        if len(auth) == 2 and auth[0].lower() == 'basic':
-            username, password = base64.b64decode(auth[1]).split(':')
-            if self.credentials.has_key(username) and self.credentials[username] == password:
-              request.META['REMOTE_USER'] = username
-              return view(request, *args, **kwargs)
+    def __call__(self, view):
+        def inner(request, *args, **kwargs):
+            # header indicates login attempt
+            if request.META.has_key('HTTP_AUTHORIZATION'):
+                auth = request.META['HTTP_AUTHORIZATION'].split()
+                if len(auth) == 2 and auth[0].lower() == 'basic':
+                    username, password = base64.b64decode(auth[1]).split(':')
+                    if self.credentials.has_key(username) and self.credentials[username] == password:
+                        request.META['REMOTE_USER'] = username
+                        return view(request, *args, **kwargs)
 
-      # The credentials are incorrect, or not provided; challenge for username/password
-      response = HttpResponse()
-      response.status_code = 401
-      response['WWW-Authenticate'] = 'Basic realm="restricted"'
-      return response
+            # The credentials are incorrect, or not provided; challenge for username/password
+            response = HttpResponse()
+            response.status_code = 401
+            response['WWW-Authenticate'] = 'Basic realm="restricted"'
+            return response
 
-    return inner
+        return inner
